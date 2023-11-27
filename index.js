a50d12864f2db3f1272421a19f4166b4cc0e2305
Gonzalo Benites <benitesgonzalogaston@gmail.com>
2023-11-26 21:56:28 -0300

    Agregado de vistas

--- index.js
+++ index.js
@@ -65,7 +65,161 @@ app.get('/', function (req, res) {
         //modo: req.cookies.modo
       });      
 });
-
+app.get('/login-reloj', function (req, res) {
+    res.render("login_reloj_sin_main")
+  })
+app.post('/login-reloj', function(req, res){
+    console.log(req.body.id +' '+ req.body.password);
+    if(!req.body.id || !req.body.password){
+        res.render('login', {message: "Por favor, introduce tanto el ID como la contraseña"});
+    } else {
+        Usuarios_reloj.findOne({id: req.body.id}).then((resBuscUno) => {
+        if (resBuscUno == null) {
+            res.render('signup_reloj_sin_main', {message: "Usuario no encontrado. Regístrese"})
+        } else {
+            if(resBuscUno.id === req.body.id && resBuscUno.password === req.body.password){
+            req.session.user = resBuscUno;
+            res.redirect('/plantilla_sin_main_protegida_reloj');
+            } else {
+            res.render('login_reloj_sin_main', {message: "Credenciales no válidas."});
+            }
+        }
+        
+        
+        });
+    }
+});
+function checkSignIn(req, res, next){
+    if(req.session.user){
+        next(); //Si la sesión existe, continúa a la página
+    } else {
+        var err = new Error("No has iniciado sesión.");
+        console.log(req.session.user);
+        next(err); //Error, intentando acceder a una página no autorizada
+    }
+}
+app.get('/plantilla_sin_main_protegida_reloj', checkSignIn, function(req, res){
+  res.render('plantilla_sin_main_protegida_reloj', {
+    id: req.session.user.id,
+    //id: req.session.user.preferencias.color_fondo,
+    color_fondo: req.session.user.preferencias.color_fondo,
+    color_fuente: req.session.user.preferencias.color_fuente,
+    tamano_hora: req.session.user.preferencias.tamaño_hora,
+    tamaño_segundos: req.session.user.preferencias.tamaño_segundos,
+    tamaño_fecha: req.session.user.preferencias.tamaño_fecha,
+  })
+});
+app.get('/logout', function(req, res){
+  req.session.destroy(function(){
+     console.log("Usuario desconectado.")
+  });
+  res.redirect('/');
+});
+app.get('/registrarse', function(req, res){
+    res.render('signup_reloj_sin_main');
+});
+app.post('/registrarse', function(req, res){
+  var reqBody = req.body;
+  if (!reqBody.id || !reqBody.password) {
+    res.render('mostrar_mensaje', {
+      mensaje: "Lo siento, proporcionaste información incorrecta", 
+      tipo: "error"
+    });
+  } else {
+    Usuarios_reloj.findOne({id: reqBody.id}).then((resBuscUno) => {
+      if (resBuscUno) {
+        // Si el usuario ya existe, muestra un mensaje de error
+        res.render('mostrar_mensaje', {
+          mensaje: "El usuario ya existe, inicie sesión", 
+          tipo: "error"
+        });
+      } else {
+        // Si el usuario no existe, crea uno nuevo
+        var newUser = new Usuarios_reloj({
+          id: reqBody.id,
+          password: reqBody.password,
+          preferencias: {
+            color_fondo: '#000',
+            color_fuente: '#00ff00',
+            tamaño_hora: 90,
+            tamaño_segundos: 45,
+            tamaño_fecha: 25
+          }
+        });
+  
+        newUser.save().then(() => {
+          res.render('login_reloj_sin_main', {
+            mensaje: "Usuario creado correctamente. Inicie sesión", 
+            tipo: "éxito", 
+            persona: reqBody
+          });
+          console.log('Document saved successfully');
+        }).catch(err => {
+          res.render('mostrar_mensaje', {
+            mensaje: "Error de base de datos", 
+            tipo: "error"
+          });
+          console.error('Error saving document:', err);
+        });
+      }
+    }).catch(err => {
+      // Manejar errores de la búsqueda de usuario
+      res.render('mostrar_mensaje', {
+        mensaje: "Error de base de datos", 
+        tipo: "error"
+      });
+      console.error('Error searching for document:', err);
+    });
+    console.log(reqBody);
+  }
+});
+app.use('/plantilla_sin_main_protegida_reloj', function(err, req, res, next){
+    console.log(err);
+    //El usuario debe estar autenticado. Redirígelo para iniciar sesión.
+    res.redirect('/login');
+});
+  router.get('/signup-reloj', function (req, res) {
+    res.render("signup_reloj_sin_main")
+  });
+  router.post('/editar-fondo/:id', async function(req, res){
+    const Usuarios_reloj = mongoose.model('Usuarios_reloj');
+    const persona_encontrada = await Usuarios_reloj.findOneAndUpdate({id: req.params.id}, {
+      preferencias: {
+        color_fondo: req.body.color_fondo
+      }
+    });
+    if (persona_encontrada){
+      console.log('Color de fondo '+req.body.color_fondo+' guardado correctamente')
+    }else{
+      console.log('Error: no se encontró el usuario');
+    }
+  });
+  router.post('/editar-fuente/:id', async function(req, res){
+    const Usuarios_reloj = mongoose.model('Usuarios_reloj');
+    const persona_encontrada = await Usuarios_reloj.findOneAndUpdate({id: req.params.id}, {
+      preferencias: {
+        color_fuente: req.body.color_fuente
+      }
+    });
+    if (persona_encontrada){
+      console.log('Color de fuente '+req.body.color_fuente+ 'guardado correctamente')
+    }else{
+      console.log('Error: no se encontró el usuario');
+    }
+  });
+  router.post('/editar-hora/:id', async function(req, res){
+    const Usuarios_reloj = mongoose.model('Usuarios_reloj');
+    const persona_encontrada = await Usuarios_reloj.findOneAndUpdate({id: req.params.id}, {
+      preferencias: {
+        color_fuente: req.body.tamano_hora
+      }
+    });
+    if (persona_encontrada){
+      console.log('Color de fuente '+req.body.tamano_hora+ 'guardado correctamente')
+    }else{
+      console.log('Error: no se encontró el usuario');
+    }
+  });
 app.listen(PORT, function () {
     console.log(`El servidor está escuchando en el puerto ${PORT}`);
 });
